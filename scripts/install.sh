866eccb0b3953269fe6db4053b5bbbd125ba7c29
Pirkka Jokela <pirkka.jokela@iki.fi>
2016-04-18 18:28:37 +0300

    Installation scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@ sudo stop TheRadiator
 ./activator stage
 sudo rm -rf /opt/TheRadiator-0.1
 sudo cp -r target/universal/stage /opt/TheRadiator-0.1
-sudo cp TheRadiator.conf /etc/init/
+sudo cp scripts/TheRadiator.conf /etc/init/
 sudo cp $1 /opt/TheRadiator-0.1/conf/application.conf
 sudo chown -R pi:users /opt/TheRadiator-0.1
 sudo start TheRadiator
